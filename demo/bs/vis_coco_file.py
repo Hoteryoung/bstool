45185306f3f09284f7b8bc070198af84e89d5424
jwwangchn <jwwangchn@outlook.com>
2020-06-20 12:29:57 +0800

    add coco vis codes

---
+++ demo/bs/vis_coco_file.py
@@ -0,0 +1,16 @@
+import os
+
+import bstool
+
+
+if __name__ == '__main__':
+    image_dir = './data/buildchange/v1/shanghai/images'
+    anno_file = './data/buildchange/v1/coco/annotations/buildchange_v1_trainval_shanghai.json'
+
+    coco_parser = bstool.COCOParse(anno_file)
+
+    for image_name in os.listdir(image_dir):
+        anns = coco_parser(image_name)
+
+        image_file = os.path.join(image_dir, image_name)
+        bstool.show_coco_mask(coco_parser.coco, image_file, anns)
