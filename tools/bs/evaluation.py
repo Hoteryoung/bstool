bd01f7191b01c61adb7c1678096b451fdd05b7d8
jwwangchn <jwwangchn@outlook.com>
2020-07-07 23:09:49 +0800

    normal sync codes

--- tools/bs/evaluation.py
+++ tools/bs/evaluation.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         pkl_file = f'../mmdetv2-bc/results/buildchange/{model}/{model}_{city}_coco_results.pkl'
         
         gt_roof_csv_file = f'./data/buildchange/v0/{city}/{city}_2048_roof_gt.csv'
-        gt_footprint_csv_file = f'/data/buildchange/v0/{city}/{city}_2048_footprint_gt.csv'
+        gt_footprint_csv_file = f'./data/buildchange/v0/{city}/{city}_2048_footprint_gt.csv'
         
         roof_csv_file = f'../mmdetv2-bc/results/buildchange/{model}/{model}_roof_merged.csv'
         rootprint_csv_file = f'../mmdetv2-bc/results/buildchange/{model}/{model}_footprint_merged.csv'
