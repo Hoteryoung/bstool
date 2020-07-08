86ce6f380c3cdc725de67454c8cc10a29d5b33a3
jwwangchn <jwwangchn@outlook.com>
2020-07-08 21:32:59 +0800

    normal sync codes

--- tools/bs/statistic.py
+++ tools/bs/statistic.py
@@ -4,7 +4,8 @@ import bstool
 if __name__ == '__main__':
     ann_file = '/data/buildchange/v1/coco/annotations/buildchange_v1_train_shanghai.json'
     
-    cities = ['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu']
+    # cities = ['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu']
+    cities = ['dalian_fine']
 #  'xian_fine', 'dalian_fine'
     csv_files = []
     for city in cities:
