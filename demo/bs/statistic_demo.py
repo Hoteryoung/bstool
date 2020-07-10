59d526db26c64ed5da7ef65313e9b5d42eaf8721
jwwangchn <jwwangchn@outlook.com>
2020-07-10 08:38:05 +0800

    normal sync codes

--- demo/bs/statistic_demo.py
+++ demo/bs/statistic_demo.py
@@ -6,13 +6,14 @@ if __name__ == '__main__':
     
     cities = ['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu']
 #  'xian_fine', 'dalian_fine'
-    csv_files = []
-    title = []
     for city in cities:
+        csv_files = []
+        title = []
+        
         csv_file = f'./data/buildchange/v0/{city}/{city}_2048_footprint_gt.csv'
         csv_files.append(csv_file)
         title.append(city)
 
-    statistic = bstool.Statistic(ann_file=None, csv_file=csv_files)
-    
-    statistic.height(title)
+        statistic = bstool.Statistic(ann_file=None, csv_file=csv_files)
+        
+        statistic.height(title)
