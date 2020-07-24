eed465b6e2e0356990832c2239a98c881a3bb63c
jwwangchn <jwwangchn@outlook.com>
2020-07-24 11:33:40 +0800

    normal sync codes

--- demo/bs/evaluation_demo.py
+++ demo/bs/evaluation_demo.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
                 anno_file = f'./data/buildchange/v1/coco/annotations/buildchange_v1_{imageset}_{city}_fine.json'
                 gt_roof_csv_file = './data/buildchange/v0/xian_fine/xian_fine_2048_roof_gt.csv'
                 gt_footprint_csv_file = './data/buildchange/v0/xian_fine/xian_fine_2048_footprint_gt.csv'
-            if city == 'xian_fixed':
+            elif city == 'xian_fixed':
                 imageset = 'val'
                 anno_file = f'./data/buildchange/v1/coco/annotations/buildchange_v1_{imageset}_{city}_fine.json'
                 gt_roof_csv_file = './data/buildchange/v0/xian_fine/xian_fine_2048_roof_gt_fixed.csv'
