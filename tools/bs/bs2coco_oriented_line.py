a89de79aa0e3fc7948a01727bae752427862acf6
jwwangchn <jwwangchn@outlook.com>
2020-07-27 17:56:19 +0800

    normal sync codes

--- tools/bs/bs2coco_oriented_line.py
+++ tools/bs/bs2coco_oriented_line.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
     # cities = ['xian_fine_origin']
     release_version = 'v1'
 
-    with_normal = True
+    with_normal = False
 
     groundtruth = True
     with_height_sample = False
