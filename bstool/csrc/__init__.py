4f03a39b853cd08012d37cffef45ddef71764545
jwwangchn <jwwangchn@outlook.com>
2020-07-29 23:06:58 +0800

    normal sync codes

--- bstool/csrc/__init__.py
+++
@@ -1,4 +0,0 @@
-from .nms import batched_rnms, rnms
-from .rbbox_geo import rbbox_iou_iof
-
-__all__ = ['rbbox_iou_iof', 'batched_rnms', 'rnms']
--- bstool/version.py
+++ bstool/version.py
@@ -1,4 +1,5 @@
 # GENERATED VERSION FILE
+# TIME: Wed Jul 29 23:03:36 2020
 
-__version__ = '0.0.1+8e60857'
+__version__ = '0.0.1+91ddaea'
 short_version = '0.0.1'
