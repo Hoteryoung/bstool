a25d6d56783a065c74acf2628134c2a12756a27d
jwwangchn <jwwangchn@outlook.com>
2020-08-04 16:42:24 +0800

    normal sync codes

--- bstool/datasets/dump.py
+++ bstool/datasets/dump.py
@@ -80,6 +80,55 @@ def bs_json_dump(polygons, properties, image_info, json_file):
         json.dump(json_data, jsonfile, indent=4)
 
 
+def urban3d_json_dump(polygons, properties, image_info, json_file):
+    """dump json file designed for building segmentation
+
+    Args:
+        polygons (list): list of polygons
+        properties (list): list of property
+        image_info (dict): image information
+        json_file (str): json file name
+    """
+    annos = []
+    for idx, (roof_polygon, single_property) in enumerate(zip(polygons, properties)):
+        object_struct = dict()
+        if roof_polygon.geom_type == 'MultiPolygon':
+            for roof_polygon_ in roof_polygon:
+                if roof_polygon_.area < 20:
+                    continue
+                else:
+                    object_struct['roof'] = bstool.polygon2mask(roof_polygon_)
+                    xoffset, yoffset = single_property['offset']
+                    transform_matrix = [1, 0, 0, 1, xoffset, yoffset]
+                    footprint_polygon = affinity.affine_transform(roof_polygon_, transform_matrix)
+                    building_height = single_property['building_height']
+                    object_struct['footprint'] = bstool.polygon2mask(footprint_polygon)
+                    object_struct['offset'] = [xoffset, yoffset]
+                    object_struct['building_height'] = building_height
+
+                    annos.append(object_struct)
+        elif roof_polygon.geom_type == 'Polygon':
+            object_struct['roof'] = bstool.polygon2mask(roof_polygon)
+            xoffset, yoffset = single_property['offset']
+            transform_matrix = [1, 0, 0, 1, xoffset, yoffset]
+            footprint_polygon = affinity.affine_transform(roof_polygon, transform_matrix)
+            building_height = single_property['building_height']
+            object_struct['footprint'] = bstool.polygon2mask(footprint_polygon)
+            object_struct['offset'] = [xoffset, yoffset]
+            object_struct['building_height'] = building_height
+        
+            annos.append(object_struct)
+        else:
+            continue
+            # print("Runtime Warming: This processing do not support {}".format(type(roof_polygon)))
+
+    json_data = {"image": image_info,
+                "annotations": annos
+                }
+
+    with open(json_file, "w") as jsonfile:
+        json.dump(json_data, jsonfile, indent=4)
+
 def bs_csv_dump(objects, roof_csv_file, footprint_csv_file):
     ori_image_name_list = list(objects.keys())
 
