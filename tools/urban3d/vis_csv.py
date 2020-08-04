c02d0116e550c6f89a1e93d13e941f3d80394459
jwwangchn <jwwangchn@outlook.com>
2020-08-04 19:28:30 +0800

    normal sync codes

--- tools/urban3d/vis_csv.py
+++ tools/urban3d/vis_csv.py
@@ -8,7 +8,7 @@ import cv2
 if __name__ == '__main__':
 
     image_dir = '/data/urban3d/v1/val/images'
-    csv_df = pandas.read_csv('/data/urban3d/v0/val/urban3d_2048_roof_gt.csv')
+    csv_df = pandas.read_csv('/data/urban3d/v0/val/urban3d_2048_footprint_gt.csv')
 
     for image_name in os.listdir(image_dir):
         image_file = os.path.join(image_dir, image_name)
@@ -18,7 +18,12 @@ if __name__ == '__main__':
  
         roof_masks = []
         for idx, row in csv_df[csv_df.ImageId == image_basename].iterrows():
-            roof_polygon = shapely.wkt.loads(row.PolygonWKT_Pix)
+            if type(row.PolygonWKT_Pix) == str:
+                roof_polygon = shapely.wkt.loads(row.PolygonWKT_Pix)
+            else:
+                roof_polygon = row.PolygonWKT_Pix
+
+            print(roof_polygon)
 
             roof_mask = bstool.polygon2mask(roof_polygon)
             roof_masks.append(roof_mask)
@@ -26,5 +31,5 @@ if __name__ == '__main__':
         if len(roof_masks) == 0:
             continue
         
-        img = bstool.draw_mask_boundary(img, roof_masks)
+        img = bstool.draw_masks_boundary(img, roof_masks)
         bstool.show_image(img)
