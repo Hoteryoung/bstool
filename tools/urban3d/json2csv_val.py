4478e5490bc8c607d3a004a66cfa313d9c9240de
jwwangchn <jwwangchn@outlook.com>
2020-08-05 09:41:26 +0800

    normal sync codes

--- tools/urban3d/json2csv_val.py
+++ tools/urban3d/json2csv_val.py
@@ -32,57 +32,61 @@ def parse_json(json_file):
 
 if __name__ == '__main__':
     imagesets = ['val', 'train']
+    datasets = ['JAX_OMA', 'ATL']
     for imageset in imagesets:
-        roof_csv_file = f'./data/urban3d/v0/{imageset}/urban3d_2048_roof_gt.csv'
-        footprint_csv_file = f'./data/urban3d/v0/{imageset}/urban3d_2048_footprint_gt.csv'
-        
-        first_in = True
-        min_area = 100
+        for dataset in datasets:
+            roof_csv_file = f'./data/urban3d/v0/{imageset}/urban3d_2048_{dataset}_roof_gt.csv'
+            footprint_csv_file = f'./data/urban3d/v0/{imageset}/urban3d_2048_{dataset}_footprint_gt.csv'
+            
+            first_in = True
+            min_area = 100
 
-        generation_mode = 'labels_footprint_pixel_roof_mean'
+            generation_mode = 'labels_footprint_pixel_roof_mean'
 
-        json_dir = f'./data/urban3d/v1/{imageset}/{generation_mode}'
+            json_dir = f'./data/urban3d/v1/{imageset}/{generation_mode}'
 
-        json_file_list = os.listdir(json_dir)
-        for json_file in json_file_list:
-            base_name = bstool.get_basename(json_file)
-
-            json_file = os.path.join(json_dir, json_file)
-            objects = parse_json(json_file)
-
-            roof_gt_polygons, footprint_gt_polygons, gt_heights, gt_offsets = [], [], [], []
-            for obj in objects:
-                roof_gt_polygon = obj['roof_polygon']
-                footprint_gt_polygon = obj['footprint_polygon']
-                gt_height = obj['building_height']
-                gt_offset = obj['offset']
-                if roof_gt_polygon.area < min_area:
+            json_file_list = os.listdir(json_dir)
+            for json_file in json_file_list:
+                base_name = bstool.get_basename(json_file)
+                if base_name.split('_')[0] not in dataset:
                     continue
 
-                roof_gt_polygons.append(roof_gt_polygon)
-                footprint_gt_polygons.append(footprint_gt_polygon)
-                gt_offsets.append(gt_offset)
-                gt_heights.append(gt_height)
+                json_file = os.path.join(json_dir, json_file)
+                objects = parse_json(json_file)
 
-            roof_csv_image = pandas.DataFrame({'ImageId': base_name.replace('JSON', 'RGB'),
-                                            'BuildingId': range(len(roof_gt_polygons)),
-                                            'PolygonWKT_Pix': roof_gt_polygons,
-                                            'Confidence': 1,
-                                            'Offset': gt_offsets,
-                                            'Height': gt_heights})
-            footprint_csv_image = pandas.DataFrame({'ImageId': base_name.replace('JSON', 'RGB'),
-                                            'BuildingId': range(len(footprint_gt_polygons)),
-                                            'PolygonWKT_Pix': footprint_gt_polygons,
-                                            'Confidence': 1,
-                                            'Offset': gt_offsets,
-                                            'Height': gt_heights})
-            if first_in:
-                roof_csv_dataset = roof_csv_image
-                footprint_csv_dataset = footprint_csv_image
-                first_in = False
-            else:
-                roof_csv_dataset = roof_csv_dataset.append(roof_csv_image)
-                footprint_csv_dataset = footprint_csv_dataset.append(footprint_csv_image)
+                roof_gt_polygons, footprint_gt_polygons, gt_heights, gt_offsets = [], [], [], []
+                for obj in objects:
+                    roof_gt_polygon = obj['roof_polygon']
+                    footprint_gt_polygon = obj['footprint_polygon']
+                    gt_height = obj['building_height']
+                    gt_offset = obj['offset']
+                    if roof_gt_polygon.area < min_area:
+                        continue
 
-        roof_csv_dataset.to_csv(roof_csv_file, index=False)
-        footprint_csv_dataset.to_csv(footprint_csv_file, index=False)
+                    roof_gt_polygons.append(roof_gt_polygon)
+                    footprint_gt_polygons.append(footprint_gt_polygon)
+                    gt_offsets.append(gt_offset)
+                    gt_heights.append(gt_height)
+
+                roof_csv_image = pandas.DataFrame({'ImageId': base_name.replace('JSON', 'RGB'),
+                                                'BuildingId': range(len(roof_gt_polygons)),
+                                                'PolygonWKT_Pix': roof_gt_polygons,
+                                                'Confidence': 1,
+                                                'Offset': gt_offsets,
+                                                'Height': gt_heights})
+                footprint_csv_image = pandas.DataFrame({'ImageId': base_name.replace('JSON', 'RGB'),
+                                                'BuildingId': range(len(footprint_gt_polygons)),
+                                                'PolygonWKT_Pix': footprint_gt_polygons,
+                                                'Confidence': 1,
+                                                'Offset': gt_offsets,
+                                                'Height': gt_heights})
+                if first_in:
+                    roof_csv_dataset = roof_csv_image
+                    footprint_csv_dataset = footprint_csv_image
+                    first_in = False
+                else:
+                    roof_csv_dataset = roof_csv_dataset.append(roof_csv_image)
+                    footprint_csv_dataset = footprint_csv_dataset.append(footprint_csv_image)
+
+            roof_csv_dataset.to_csv(roof_csv_file, index=False)
+            footprint_csv_dataset.to_csv(footprint_csv_file, index=False)
