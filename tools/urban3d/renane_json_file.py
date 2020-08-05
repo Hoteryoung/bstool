f4bb55bc34c856257d7ebefbbbff298329497aa0
jwwangchn <jwwangchn@outlook.com>
2020-08-05 11:50:07 +0800

    normal sync codes

--- tools/urban3d/renane_json_file.py
+++ tools/urban3d/renane_json_file.py
@@ -4,14 +4,16 @@ import os
 if __name__ == '__main__':
     versions = ['v1', 'v2']
     imagesets = ['val', 'train']
+    folds = ['lables', 'images']
     
     root_dir = './data/urban3d'
 
     for version in versions:
         for imageset in imagesets:
-            label_dir = os.path.join(root_dir, version, imageset, 'labels')
-            for label_file in os.listdir(label_dir):
-                if 'JSON' in label_file:
-                    old_file = os.path.join(label_dir, label_file)
-                    new_file = os.path.join(label_dir, label_file.replace('JSON', 'RGB'))
-                    os.rename(old_file, new_file)
+            for fold in folds:
+                dst_dir = os.path.join(root_dir, version, imageset, fold)
+                for label_file in os.listdir(dst_dir):
+                    if 'JSON' in label_file:
+                        old_file = os.path.join(dst_dir, label_file)
+                        new_file = os.path.join(dst_dir, label_file.replace('JSON', 'RGB'))
+                        os.rename(old_file, new_file)
