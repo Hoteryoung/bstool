e5a8714b8215925a1f349aae54d2d5e7bd1af741
jwwangchn <jwwangchn@outlook.com>
2020-08-12 08:36:09 +0800

    normal sync codes

--- tools/urban3d/merge_csv_file.py
+++ tools/urban3d/merge_csv_file.py
@@ -5,11 +5,12 @@ import bstool
 
 if __name__ == '__main__':
     csv_dir = './data/urban3d/weijia/instance_gt_val'
+    save_dir = './data/urban3d/weijia'
 
     csv_fns = ['urban3d_jax_val_orgfootprint_offset_gt_subcsv_merge.csv',
                'urban3d_oma_val_orgfootprint_offset_gt_subcsv_merge.csv']
 
-    merged_csv_file = os.path.join(csv_dir, 'urban3d_jax_oma_val_orgfootprint_offset_gt_subcsv_merge.csv')
+    merged_csv_file = os.path.join(save_dir, 'urban3d_jax_oma_val_orgfootprint_offset_gt_subcsv_merge.csv')
     first_in = True
     for csv_fn in csv_fns:
         csv_file = os.path.join(csv_dir, csv_fn)
