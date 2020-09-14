8f0efc52caefe38460cb2d14785d102d8b281bc3
jwwangchn <jwwangchn@outlook.com>
2020-09-14 15:14:40 +0800

    normal sync codes

---
+++ tools/bs/dataset/fix_empty_side_face.py
@@ -0,0 +1,30 @@
+import os
+import cv2
+import bstool
+
+
+if __name__ == '__main__':
+    core_dataset_name = 'buildchange'
+
+    cities = ['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu']
+
+    src_version = 'v1'
+
+    for city in cities:
+        image_dir = f'./data/{core_dataset_name}/{src_version}/{city}/images'
+        side_face_image_dir = f'./data/{core_dataset_name}/{src_version}/{city}/side_face'
+
+        side_face_image_list = os.listdir(side_face_image_dir)
+        image_list = os.listdir(image_dir)
+
+        for image_name in side_face_image_list:
+            # empty_edge_map = bstool.generate_image(1024, 1024, 0)
+            
+            side_face_file = os.path.join(side_face_image_dir, image_name)
+            side_face = cv2.imread(side_face_file)
+
+            if side_face.sum() == 0:
+                print(f"generate empty side face image: {side_face_file}")
+            
+            # cv2.imwrite(side_face_file, empty_edge_map)
+            
