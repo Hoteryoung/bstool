9ed9d67c5474599ad82b5869e91171a2219bf96b
jwwangchn <jwwangchn@outlook.com>
2020-09-24 00:01:56 +0800

    normal sync codes

--- demo/bs/evaluation_demo.py
+++ demo/bs/evaluation_demo.py
@@ -80,19 +80,21 @@ ALL_MODELS = ['bc_v005.01_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_buildin
             'bc_v010.15.01_real_semi_supervised_offset_rcnn_r50_1x_v1_semi_urban3d_bc_wo_building_bbox_rpn',
             'bc_v010.15.02_real_semi_supervised_offset_rcnn_r50_1x_v1_semi_urban3d_bc_wo_building_bbox_rpn_pretrain',
             'bc_v010.15.03_real_semi_supervised_offset_rcnn_r50_1x_v1_semi_urban3d_bc_wo_building_bbox_rpn_pretrain_lr0.04',
+            'bc_v010.16.01_real_semi_supervised_offset_rcnn_r50_1x_v1_bc_wo_building_bbox_rpn',
+            'bc_v010.16.02_real_semi_supervised_offset_rcnn_r50_1x_v1_bc_wo_building_bbox_rpn_pretrain'
+            'bc_v010.16.03_real_semi_supervised_offset_rcnn_r50_1x_v1_bc_wo_building_bbox_rpn_pretrain_lr0.04',
             'bc_v011.01_offset_rcnn_r50_1x_v1_with_edge',
             'bc_v012.01.01_r50_1x_v1_offset_field',
             'bc_v012.02.01_r50_1x_v1_pixel_offset',
             'bc_v013.01.01_offset_rcnn_with_side_face_reweight',
             'bc_v014.01.01_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_coordconv',
             'bc_v015.01.01_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_non_local',
-            'bc_v010.16.01_real_semi_supervised_offset_rcnn_r50_1x_v1_bc_wo_building_bbox_rpn',
-            'bc_v010.16.02_real_semi_supervised_offset_rcnn_r50_1x_v1_bc_wo_building_bbox_rpn_pretrain']
+            ]
 
 if __name__ == '__main__':
     # models = ['bc_v005.08.02_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_polar_cos_sin', 'bc_v005.08.03_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_polar_cos_sin_no_norm']
     # models = ['bc_v005.07_offset_rcnn_r50_2x_v1_5city_trainval_roof_mask_building_bbox_smooth_l1_offsetweight_2.0_conv10']
-    models = [model for model in ALL_MODELS[1:] if 'v010.15.03' in model]
+    models = [model for model in ALL_MODELS[1:] if 'v010.16.03' in model]
     # models = ['bc_v006.05_height_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_angle']
     # models = ['bc_v006.01_height_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_linear_50_50']
     # cities = ['jinan', 'shanghai', 'beijing','chengdu', 'haerbin']
