0b5cb1490e59a49302bd0696075fb4bdacebcd7b
jwwangchn <jwwangchn@outlook.com>
2020-10-04 01:17:26 +0800

    normal sync codes

--- bstool/datasets/convert2coco.py
+++ bstool/datasets/convert2coco.py
@@ -72,10 +72,7 @@ class Convert2COCO():
             annotations_coco = self.__generate_coco_annotation__(annotpath, imgpath)
 
             # if annotation is empty, skip this annotation
-            if len(annotations_coco) < 5:
-                print("name: ", name)
-                print("annotations_coco: ", annotations_coco)
-            if len(annotations_coco) != 0 or self.groundtruth == False:
+            if len(annotations_coco) > 2 or self.groundtruth == False:
                 if self.image_size is None:
                     img = cv2.imread(imgpath)
                     height, width, _ = img.shape
