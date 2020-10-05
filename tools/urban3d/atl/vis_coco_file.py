11d9d02956636a9df32243ed0faed9a125c0bbf5
jwwangchn <jwwangchn@outlook.com>
2020-10-05 10:11:12 +0800

    normal sync codes

--- tools/urban3d/atl/vis_coco_file.py
+++ tools/urban3d/atl/vis_coco_file.py
@@ -6,13 +6,14 @@ import bstool
 if __name__ == '__main__':
     # image_dir = '/data/plane/v1/train/images'
     # anno_file = '/data/plane/v1/coco/annotations/plane_train.json'
-    anno_file = '/mnt/lustre/wangjinwang/buildchange/codes/bstool/data/urban3d/v2/coco/annotations/urban3d_v2_train_ATL.jso'
+    anno_file = '/mnt/lustre/wangjinwang/buildchange/codes/bstool/data/urban3d/v2/coco/annotations/urban3d_v2_train_ATL.json'
     image_dir = '/mnt/lustre/wangjinwang/buildchange/codes/bstool/data/urban3d/v2/ATL/train/images'
     output_dir = '/mnt/lustre/wangjinwang/buildchange/codes/bstool/data/urban3d/vis/ATL/train'
 
     coco_parser = bstool.COCOParse(anno_file)
 
     for image_name in os.listdir(image_dir):
+        print(image_name)
         anns = coco_parser(image_name)
 
         image_file = os.path.join(image_dir, image_name)
