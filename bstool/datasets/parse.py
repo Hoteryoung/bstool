d8d86f4c9afdde8b1b3571cb073c60510ea5ffde
jwwangchn <jwwangchn@outlook.com>
2020-10-08 08:15:15 +0800

    normal sync codes

--- bstool/datasets/parse.py
+++ bstool/datasets/parse.py
@@ -104,7 +104,7 @@ def lingxuan_json_parser(json_file):
         yoffset = content['xyoffset'][str(idx)][1]
         ignore = content['ignoreFlag'][str(idx)]
 
-        if not (isinstance(xoffset, (float, int)) and isinstance(yoffset, (float, int)))
+        if not (isinstance(xoffset, (float, int)) and isinstance(yoffset, (float, int))):
             xoffset, yoffset = 0, 0
         object_struct['polygon'] = footprint_polygon
         object_struct['property'] = {"Id": idx, "Floor": building_height // 3, "xoffset": xoffset, "yoffset": yoffset, 'ignore': ignore}
