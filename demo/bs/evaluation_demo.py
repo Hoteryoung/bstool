b68fcc37736f316a77d957a2b92152fd7662a7d0
jwwangchn <jwwangchn@outlook.com>
2020-10-09 18:43:35 +0800

    normal sync codes

--- demo/bs/evaluation_demo.py
+++ demo/bs/evaluation_demo.py
@@ -58,6 +58,7 @@ ALL_MODELS = ['bc_v005.01_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_buildin
             'bc_v005.09.08_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_rotate_augmentation_4_angles_14_size',
             'bc_v005.09.09_offset_rcnn_r50_1x_v2_5city_trainval_roof_mask_building_bbox_rotate_augmentation_4_angles',
             'bc_v005.10.01_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_rotate_offset_feature',
+            'bc_v005.10.02_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_rotate_offset_feature_wo_online_augmentation',
             'bc_v008.01_mask_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_urban3d',
             'bc_v008.02_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_urban3d',
             'bc_v008.02.01_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_urban3d_atl',
@@ -99,7 +100,7 @@ ALL_MODELS = ['bc_v005.01_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_buildin
 if __name__ == '__main__':
     # models = ['bc_v005.08.02_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_polar_cos_sin', 'bc_v005.08.03_offset_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_polar_cos_sin_no_norm']
     # models = ['bc_v005.07_offset_rcnn_r50_2x_v1_5city_trainval_roof_mask_building_bbox_smooth_l1_offsetweight_2.0_conv10']
-    models = [model for model in ALL_MODELS[1:] if 'v008.02.01' in model]
+    models = [model for model in ALL_MODELS[1:] if 'v005.10.02' in model]
     # models = ['bc_v006.05_height_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_angle']
     # models = ['bc_v006.01_height_rcnn_r50_1x_v1_5city_trainval_roof_mask_building_bbox_linear_50_50']
     # cities = ['jinan', 'shanghai', 'beijing','chengdu', 'haerbin']
