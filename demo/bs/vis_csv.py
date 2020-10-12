005ea5ff6dac46e76ca25d23ff75b48426748d4c
jwwangchn <jwwangchn@outlook.com>
2020-10-12 21:44:41 +0800

    normal sync codes

---
+++ demo/bs/vis_csv.py
@@ -0,0 +1,35 @@
+import bstool
+import pandas
+import shapely
+import os
+import cv2
+
+
+if __name__ == '__main__':
+
+    image_dir = '/data/buildchange/v0/xian_fine/images'
+    # csv_df = pandas.read_csv('/data/urban3d/weijia/urban3d_jax_oma_val_orgfootprint_offset_gt_simple_subcsv_merge.csv')
+    csv_df = pandas.read_csv('/data/buildchange/v0/xian_fine/xian_val_footprint_gt_minarea100_26.csv')
+
+
+    for image_name in os.listdir(image_dir):
+        image_file = os.path.join(image_dir, image_name)
+        image_basename = bstool.get_basename(image_name)
+
+        img = cv2.imread(image_file)
+ 
+        roof_masks = []
+        for idx, row in csv_df[csv_df.ImageId == image_basename].iterrows():
+            if type(row.PolygonWKT_Pix) == str:
+                roof_polygon = shapely.wkt.loads(row.PolygonWKT_Pix)
+            else:
+                roof_polygon = row.PolygonWKT_Pix
+
+            roof_mask = bstool.polygon2mask(roof_polygon)
+            roof_masks.append(roof_mask)
+
+        if len(roof_masks) == 0:
+            continue
+        
+        img = bstool.draw_masks_boundary(img, roof_masks)
+        bstool.show_image(img)
