c98c64840bbda173ef2a5f2454348542d59e484d
jwwangchn <jwwangchn@outlook.com>
2020-10-26 00:49:46 +0800

    normal sync codes

--- tools/bs/dataset/image_num_height_angle_1024.py
+++ tools/bs/dataset/image_num_height_angle_1024.py
@@ -114,13 +114,13 @@ class CountImage():
                         'no_ignore_rate': 0.9}
         else:
             parameters = {
-                        'angles': 20,
+                        'angles': 10,
                         "mean_height": 4,
-                        "mean_angle": 45, 
+                        "mean_angle": 40, 
                         "mean_offset_length": 10,
                         'std_offset_length': 5,
-                        'std_angle': 10,
-                        'no_ignore_rate': 0.9}
+                        'std_angle': 15,
+                        'no_ignore_rate': 0.80}
         
         angles = np.abs(angles) * 180.0 / math.pi
         offset_lengths = np.abs(offset_lengths)
