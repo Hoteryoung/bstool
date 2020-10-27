0f3914c6a8e7574b2867ffffbc56d5db68719eb9
jwwangchn <jwwangchn@outlook.com>
2020-10-27 17:33:59 +0800

    normal sync codes

--- tools/bs/dataset/select_high_score_vis_images.py
+++ tools/bs/dataset/select_high_score_vis_images.py
@@ -18,12 +18,9 @@ def parse_csv(csv_file):
 
 if __name__ == '__main__':
     vis_root_dir = './data/buildchange/public/20201027/vis/{}'
-    random_vis_dir = './data/buildchange/public/20201027/random_vis'
     csv_file = './data/buildchange/public/misc/nooverlap/training_dataset_info_20201027.csv'
 
     file_names, scores = parse_csv(csv_file)
-
-    bstool.mkdir_or_exist(random_vis_dir)
 
     cities = ['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu']
     sub_folds = {'beijing':  ['arg', 'google', 'ms'],
@@ -41,18 +38,26 @@ if __name__ == '__main__':
                 vis_file = os.path.join(vis_dir, basename + '.png')
                 vis_file_list.append(vis_file)
 
-    
-    random.seed(0)
-    vis_file_list.sort()
-    random.shuffle(vis_file_list)
+    sorted_index = np.argsort(scores)[::-1]
+    file_names = [file_names[idx] for idx in sorted_index]
+    scores = [scores[idx] for idx in sorted_index]
 
+    random_vis_dir = './data/buildchange/public/20201027/high_score_image_300'
+
+    bstool.mkdir_or_exist(random_vis_dir)
+    
     max_score, min_score = max(scores), min(scores)
 
-    for vis_file in vis_file_list[0:300]:
+    high_score_300_fns = file_names[0:300]
+
+    for vis_file in vis_file_list:
         basename = bstool.get_basename(vis_file)
+
+        if basename not in high_score_300_fns:
+            continue
 
         score = (scores[file_names.index(basename)] - min_score) / (max_score - min_score) * 100 + 100
 
-        shutil.copy(vis_file, os.path.join(random_vis_dir, str(int(score) + '_' + basename))
+        shutil.copy(vis_file, os.path.join(random_vis_dir, str(int(score)) + '_' + basename))
 
     
