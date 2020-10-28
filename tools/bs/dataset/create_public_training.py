296b4d5d7c32ce628e7d412eea0022cb1979a4c4
jwwangchn <jwwangchn@outlook.com>
2020-10-28 21:36:43 +0800

    normal sync codes

--- tools/bs/dataset/create_public_training.py
+++ tools/bs/dataset/create_public_training.py
@@ -22,35 +22,57 @@ def parse_csv(csv_file):
 
     return file_names, ori_image_names, scores, full_data
 
+def keep_ori_image(ori_image_name, 
+                   file_names, 
+                   candidate_coords=[(0, 0), (0, 1024), (1024, 0), (1024, 1024)], 
+                   cities=['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu'], 
+                   score_threshold_index=10000,
+                   keep_sub_image_num_threshold=4):
+    training_info = []
+    for city in cities:
+        sub_image_num = 0
+        for candidate_coord in candidate_coords:
+            arg = f"{city}_arg__{ori_image_name}__{candidate_coord[0]}_{candidate_coord[1]}"
+            google = f"{city}_google__{ori_image_name}__{candidate_coord[0]}_{candidate_coord[1]}"
+            ms = f"{city}_ms__{ori_image_name}__{candidate_coord[0]}_{candidate_coord[1]}"
+
+            if arg in file_names and google in file_names and ms in file_names:
+                try:
+                    arg_idx, google_idx, ms_idx = file_names.index(arg, 0, score_threshold_index), file_names.index(google, 0, score_threshold_index), file_names.index(ms, 0, score_threshold_index)
+                except:
+                    continue
+                
+                sub_image_num += 1
+    
+                training_info.append(full_data[arg_idx])
+                training_info.append(full_data[google_idx])
+                training_info.append(full_data[ms_idx])
+
+    if sub_image_num >= keep_sub_image_num_threshold * 3:
+        return training_info
+    else:
+        return
+
+
 if __name__ == '__main__':
     csv_file = './data/buildchange/public/misc/nooverlap/full_dataset_info.csv'
     candidate_coords = [(0, 0), (0, 1024), (1024, 0), (1024, 1024)]
     cities = ['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu']
+    version = '20201027'
 
     file_names, ori_image_names, scores, full_data = parse_csv(csv_file)
 
-    score_threshold_index = 9360
+    score_threshold_index = 15000
     
     training_info = []
     for ori_image_name, score in zip(ori_image_names, scores):
-        for candidate_coord in candidate_coords:
-            for city in cities:
-                arg = f"{city}_arg__{ori_image_name}__{candidate_coord[0]}_{candidate_coord[1]}"
-                google = f"{city}_google__{ori_image_name}__{candidate_coord[0]}_{candidate_coord[1]}"
-                ms = f"{city}_ms__{ori_image_name}__{candidate_coord[0]}_{candidate_coord[1]}"
-
-                if arg in file_names and google in file_names and ms in file_names:
-                    try:
-                        arg_idx, google_idx, ms_idx = file_names.index(arg, 0, score_threshold_index), file_names.index(google, 0, score_threshold_index), file_names.index(ms, 0, score_threshold_index)
-                    except:
-                        continue
-                
-                    training_info.append(full_data[arg_idx])
-                    training_info.append(full_data[google_idx])
-                    training_info.append(full_data[ms_idx])
-
+        result = keep_ori_image(ori_image_name, file_names, candidate_coords, score_threshold_index, keep_sub_image_num_threshold=4)
+        if result is None:
+            continue
+        else:
+            training_info.extend(result)
     
-    training_csv_file = './data/buildchange/public/misc/nooverlap/training_dataset_info_20201027.csv'
+    training_csv_file = f'./data/buildchange/public/misc/nooverlap/training_dataset_info_{version}.csv'
     with open(training_csv_file, 'w') as f:
         csv_writer = csv.writer(f, delimiter=',')
         head = ['file_name', 'sub_fold', 'ori_image_fn', 'coord_x', 'coord_y', 'object_num', 'mean_angle', 'mean_height', 'mean_offset_length', 'std_offset_length', 'std_angle', 'no_ignore_rate', 'score']
@@ -59,22 +81,23 @@ if __name__ == '__main__':
             csv_writer.writerow(data)
 
     print("The number of training data: ", len(training_info))
+    
     if len(training_info) == 3000:
         src_root_image_dir = './data/buildchange/v2/{}/images'
         src_root_label_dir = './data/buildchange/v2/{}/labels'
-        dst_root_image_dir = './data/buildchange/public/20201027/{}/images'
-        dst_root_label_dir = './data/buildchange/public/20201027/{}/labels'
+        dst_root_image_dir = './data/buildchange/public/{}/{}/images'
+        dst_root_label_dir = './data/buildchange/public/{}/{}/labels'
         for data in tqdm.tqdm(training_info):
             base_name = data[0]
             city = base_name.split("__")[0].split('_')[0]
             src_image_file = os.path.join(src_root_image_dir.format(city), base_name + '.png')
             src_label_file = os.path.join(src_root_label_dir.format(city), base_name + '.json')
 
-            bstool.mkdir_or_exist(dst_root_image_dir.format(city))
-            bstool.mkdir_or_exist(dst_root_label_dir.format(city))
+            bstool.mkdir_or_exist(dst_root_image_dir.format(version, city))
+            bstool.mkdir_or_exist(dst_root_label_dir.format(version, city))
 
-            dst_image_file = os.path.join(dst_root_image_dir.format(city), base_name + '.png')
-            dst_label_file = os.path.join(dst_root_label_dir.format(city), base_name + '.json')
+            dst_image_file = os.path.join(dst_root_image_dir.format(version, city), base_name + '.png')
+            dst_label_file = os.path.join(dst_root_label_dir.format(version, city), base_name + '.json')
             
             shutil.copy(src_image_file, dst_image_file)
             shutil.copy(src_label_file, dst_label_file)
