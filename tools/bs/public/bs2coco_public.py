38f1dc11398c665a18f349681edf2e4fbdcb7cc6
jwwangchn <jwwangchn@outlook.com>
2020-11-07 14:35:47 +0800

    normal sync codes

--- tools/bs/public/bs2coco_public.py
+++ tools/bs/public/bs2coco_public.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     # cities = ['jinan', 'haerbin', 'chengdu']
     # cities = ['shanghai', 'beijing', 'jinan', 'haerbin', 'chengdu', 'xian_fine', 'dalian_fine']
     # cities = ['xian_fine_origin']
-    # cities = ['shanghai_fine']
+    cities = ['shanghai_fine']
     version = '20201028'
     release_version = f'public/{version}'
 
