a68193f739daddc7a62b15e126676c50fd48737c
jwwangchn <jwwangchn@outlook.com>
2020-11-07 14:27:08 +0800

    normal sync codes

--- tools/bs/public/create_public_validation_from_weijia_csv.py
+++ tools/bs/public/create_public_validation_from_weijia_csv.py
@@ -25,23 +25,23 @@ if __name__ == '__main__':
 
     src_root_image_dir = './data/buildchange/v2/shanghai/images'
     src_root_label_dir = './data/buildchange/v2/shanghai/labels'
-    dst_root_image_dir = './data/buildchange/public/{}/{}/images'
-    dst_root_label_dir = './data/buildchange/public/{}/{}/labels'
+    dst_root_image_dir = './data/buildchange/public/{}/shanghai_fine/images'
+    dst_root_label_dir = './data/buildchange/public/{}/shanghai_fine/labels'
 
     for file_name in tqdm.tqdm(file_names):
         base_name = bstool.get_basename(file_name)
-        city = file_name.split("__")[0].split('_')[0]
         sub_fold, ori_image_name, coord = bstool.get_info_splitted_imagename(base_name)
-        src_image_file = os.path.join(src_root_image_dir.format(city), base_name + '.png')
-        src_label_file = os.path.join(src_root_label_dir.format(city), base_name + '.json')
+        src_image_file = os.path.join(src_root_image_dir, base_name + '.png')
+        src_label_file = os.path.join(src_root_label_dir, base_name + '.json')
 
-        bstool.mkdir_or_exist(dst_root_image_dir.format(version, city))
-        bstool.mkdir_or_exist(dst_root_label_dir.format(version, city))
+        bstool.mkdir_or_exist(dst_root_image_dir.format(version))
+        bstool.mkdir_or_exist(dst_root_label_dir.format(version))
 
-        dst_image_file = os.path.join(dst_root_image_dir.format(version, city), base_name + '.png')
-        dst_label_file = os.path.join(dst_root_label_dir.format(version, city), base_name + '.json')
+        dst_image_file = os.path.join(dst_root_image_dir.format(version), base_name + '.png')
+        dst_label_file = os.path.join(dst_root_label_dir.format(version), base_name + '.json')
         
         # shutil.copy(src_image_file, dst_image_file)
         # shutil.copy(src_label_file, dst_label_file)
-        os.remove(dst_image_file)
-        os.remove(dst_label_file)
+        print(src_label_file, dst_label_file)
+        # os.remove(dst_image_file)
+        # os.remove(dst_label_file)
